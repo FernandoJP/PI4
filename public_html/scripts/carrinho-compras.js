ad696082fdfc78413489d469f5950bd52be9f699
fernando.jpaula2 <fernando.jpaula2@CAS554946W07I06.senacsp.edu.br>
2017-03-31 19:10:12 -0300

    Carrinho de compras

--- public_html/scripts/carrinho-compras.js
+++ public_html/scripts/carrinho-compras.js
@@ -2,19 +2,14 @@
 var app = angular.module('StarterApp', ['ngAnimate', 'ngAria', 'ngMaterial', 'ngMessages', 'ngMdIcons']);
 
 app.controller('AppCtrl', ['$scope', '$mdBottomSheet', '$mdSidenav', '$mdDialog', '$mdToast', function ($scope, $mdBottomSheet, $mdSidenav, $mdDialog, $mdToast) {
+        console.log(JSON.parse(localStorage.getItem("produtos")));
         if (JSON.parse(localStorage.getItem("produtos"))) {
             $scope.qtdProdutosCarrinho = JSON.parse(localStorage.getItem("produtos")).length; //configurar quantidade de itens no carrinho
         } else {
             $scope.qtdProdutosCarrinho = 0;
         }
 
-        $scope.carrinho = [
-            {nome: 'Uma Pergunta por Dia',
-                preco: 'R$57,00',
-                descricao: '<p>Todos os dias criamos uma imensa quantidade de registros em celulares, redes sociais e aplicativos. </p><p>No entanto, quase nunca temos o hábito de retornar a eles. <br />Às vezes podem parecer só besteiras, mas quantos desses relatos não mostrariam nosso crescimento e nossas mudanças em todos esses anos? Uma pergunta por dia convida você a registrar suas respostas a uma variedade de questões, das mais simples às mais complicadas, como “Para onde você quer fazer sua próxima viagem?” ou “Escreva a primeira linha da sua autobiografia”. </p><p>Em cada página há espaço para cinco respostas, uma por ano, ao longo de cinco anos. Com o passar do tempo, quando voltar a um dia já anotado, o dono do diário encontrará seus pensamentos anteriores, num exercício divertido e construtivo de recordar e refletir.</p>',
-                img: 'images/livro-o-poder-do-habito.jpg'
-            }
-        ];
+        $scope.carrinho = JSON.parse(localStorage.getItem("produtos"));
         $scope.toggleSidenav = function (menuId) {
             $mdSidenav(menuId).toggle();
         };
--- public_html/scripts/home.js
+++ public_html/scripts/home.js
@@ -1,116 +1,118 @@
 
-var app = angular.module('StarterApp', ['ngAnimate', 'ngAria','ngMaterial', 'ngMessages', 'ngMdIcons']);
-        app.controller('AppCtrl', ['$scope', '$mdBottomSheet', '$mdSidenav', '$mdDialog', '$mdToast','$http', function ($scope,$mdBottomSheet, $mdSidenav, $mdDialog, $mdToast,$http) {
+var app = angular.module('StarterApp', ['ngAnimate', 'ngAria', 'ngMaterial', 'ngMessages', 'ngMdIcons']);
+app.controller('AppCtrl', ['$scope', '$mdBottomSheet', '$mdSidenav', '$mdDialog', '$mdToast', '$http', function ($scope, $mdBottomSheet, $mdSidenav, $mdDialog, $mdToast, $http) {
         if (JSON.parse(localStorage.getItem("produtos"))) {
-        $scope.qtdProdutosCarrinho = JSON.parse(localStorage.getItem("produtos")).length; //configurar quantidade de itens no carrinho
+            $scope.qtdProdutosCarrinho = JSON.parse(localStorage.getItem("produtos")).length; //configurar quantidade de itens no carrinho
         } else {
-        $scope.qtdProdutosCarrinho = 0;
+            $scope.qtdProdutosCarrinho = 0;
         }
 
-        $http.get("http://67.205.164.145/api/book").then(function(response) {
-        $scope.produtos = response.data;
-                console.log(response.data);
+        $http.get("http://67.205.164.145/api/book").then(function (response) {
+            $scope.produtos = response.data;
+            console.log(response.data);
         });
 
-                $scope.showCustom = function (event, dadosProduto) {
-                $mdDialog.show({
+        $scope.showCustom = function (event, dadosProduto) {
+            $mdDialog.show({
                 clickOutsideToClose: true,
-                        scope: $scope,
-                        preserveScope: true,
-                        template: '<md-dialog class="dialog-detalhes-livro">' +
+                scope: $scope,
+                preserveScope: true,
+                template: '<md-dialog class="dialog-detalhes-livro">' +
                         '<md-dialog-content>' +
                         '<h1>' + dadosProduto.title + '</h1>' +
-                        '<img class="dialog-img-livro" src="data:image/png;base64,'+dadosProduto.image+'" title="Livro ' + dadosProduto.title + '. " alt="Livro ' + dadosProduto.author + '. "/>' +
+                        '<img class="dialog-img-livro" src="data:image/png;base64,' + dadosProduto.image + '" title="Livro ' + dadosProduto.title + '. " alt="Livro ' + dadosProduto.author + '. "/>' +
                         '<div class="dialog-descricao-livro"><p>' + dadosProduto.ISBN + '</p></div>' +
                         '</md-dialog-content>' +
                         '<md-dialog-actions layout="row">' +
                         '<div class="dialog-preco-livro">Preço:<span>' + dadosProduto.price + '</span></div>' +
                         '<span flex></span>' +
-                        '<md-button class="md-icon-button"><i class="material-icons">add_shopping_cart</i></md-button>' +
                         '<md-button class="md-raised md-primary">Comprar</md-button>' +
                         '<md-dialog-actions>' +
                         '</md-dialog>',
-                        controller: function DialogController($scope, $mdDialog) {
-                        $scope.closeDialog = function () {
+                controller: function DialogController($scope, $mdDialog) {
+                    $scope.closeDialog = function () {
                         $mdDialog.hide();
-                        };
-                        }
-                });
+                    };
                 }
-        
+            });
+        }
+
         ;
-                $scope.toggleSidenav = function (menuId) {
-                $mdSidenav(menuId).toggle();
-                };
-                $scope.alterarQtdProdutos = function (operacao) {
-                if ($scope.qtdProdutos) {
+        $scope.toggleSidenav = function (menuId) {
+            $mdSidenav(menuId).toggle();
+        };
+        $scope.alterarQtdProdutos = function (operacao) {
+            if ($scope.qtdProdutos) {
                 if (operacao == 'adicionar') {
-                $scope.qtdProdutos++;
+                    $scope.qtdProdutos++;
                 } else if (operacao == 'remover') {
-                if ($scope.qtdProdutos != 1) {
-                $scope.qtdProdutos--;
+                    if ($scope.qtdProdutos != 1) {
+                        $scope.qtdProdutos--;
+                    }
                 }
-                }
-                } else {
+            } else {
                 $scope.qtdProdutos = 1;
-                }
-                };
-                $scope.adicionarAoCarrinho = function (produto) {
-                if (JSON.parse(localStorage.getItem("produtos"))) {
+            }
+            console.log($scope.qtdProdutos);
+        };
+        $scope.adicionarAoCarrinho = function (produto) {
+            if (JSON.parse(localStorage.getItem("produtos"))) {
                 var itensAdicionados = JSON.parse(localStorage.getItem("produtos"));
-                } else {
+            } else {
                 var itensAdicionados = [];
-                }
-                itensAdicionados.push({nome: produto.nome, preco: produto.preco, img: produto.img});
-                        localStorage.setItem("produtos", JSON.stringify(itensAdicionados));
-                        $scope.qtdProdutosCarrinho = JSON.parse(localStorage.getItem("produtos")).length; //atualizando no badge de carrinhos
-                        $scope.showToast(produto.nome); //mostrando o toast com a ação de desfazer
-                };
-                $scope.showToast = function (nomeProduto) {
-                var toast = $mdToast.simple()
-                        .textContent('Livro adicionado com sucesso no carrinho. ')
-                        .action('Desfazer')
-                        .highlightAction(false)
-                        .hideDelay(5000)
-                        .position('top right');
-                        $mdToast.show(toast).then(function (response) {
+            }
+            itensAdicionados.push({name: produto.name, price: produto.price, image: produto.image});
+            localStorage.setItem("produtos", JSON.stringify(itensAdicionados));
+            $scope.qtdProdutosCarrinho = JSON.parse(localStorage.getItem("produtos")).length; //atualizando no badge de carrinhos
+            $scope.showToast(produto.nome); //mostrando o toast com a ação de desfazer
+        };
+        $scope.showToast = function (nomeProduto) {
+            var toast = $mdToast.simple()
+                    .textContent('Livro adicionado com sucesso no carrinho. ')
+                    .action('Desfazer')
+                    .highlightAction(false)
+                    .hideDelay(5000)
+                    .position('top right');
+            $mdToast.show(toast).then(function (response) {
                 if (response == 'ok') { //se clicou em desfazer 
-                console.dir(JSON.parse(localStorage.getItem("produtos")));
-                        var produtos = JSON.parse(localStorage.getItem("produtos"));
-                        for (var i = produtos.length - 1; i >= 0; i--) {
-                console.log('if ' + produtos[i].nome + ' === ' + nomeProduto);
+                    console.dir(JSON.parse(localStorage.getItem("produtos")));
+                    var produtos = JSON.parse(localStorage.getItem("produtos"));
+                    for (var i = produtos.length - 1; i >= 0; i--) {
+                        console.log('if ' + produtos[i].nome + ' === ' + nomeProduto);
                         if (produtos[i].nome == nomeProduto) {
-                produtos.splice(i, 1);
-                        break;
-                }
-                }
-                localStorage.setItem("produtos", JSON.stringify(produtos));
-                        $scope.qtdProdutosCarrinho = JSON.parse(localStorage.getItem("produtos")).length; //atualizando no badge de carrinhos
+                            produtos.splice(i, 1);
+                            break;
+                        }
+                    }
+                    localStorage.setItem("produtos", JSON.stringify(produtos));
+                    $scope.qtdProdutosCarrinho = JSON.parse(localStorage.getItem("produtos")).length; //atualizando no badge de carrinhos
                 }
 
-                });
-                };
-        }]);
-                function mostrarItensLocalStorage() {
-                console.dir(JSON.parse(localStorage.getItem("produtos")));
-                        }
-        ;
-                app.config(function ($mdThemingProvider) {
-                var customBlueMap = $mdThemingProvider.extendPalette('blue-grey', {
-                'contrastDefaultColor': 'light',
-                        'contrastDarkColors': ['50'],
-                        '50': 'ffffff'
-                });
-                        $mdThemingProvider.definePalette('customBlue', customBlueMap);
-                        $mdThemingProvider.theme('default')
-                        .primaryPalette('customBlue', {
-                        'default': '500',
-                                'hue-1': '50'
-                        })
-                        .accentPalette('light-blue', {
-                        'default': '500',
-                                'hue-1': '50'
-                        })
-                        $mdThemingProvider.theme('input', 'default')
-                        .primaryPalette('grey')
-                        });
+            });
+        };
+    }]);
+function mostrarItensLocalStorage() {
+    console.dir(JSON.parse(localStorage.getItem("produtos")));
+}
+;
+
+  app.config(function ($mdThemingProvider) {
+    var customBlueMap = $mdThemingProvider.extendPalette('blue-grey', {
+        'contrastDefaultColor': 'light',
+        'contrastDarkColors': ['50'],
+        '50': 'ffffff'
+    });
+    $mdThemingProvider.definePalette('customBlue', customBlueMap);
+    $mdThemingProvider.theme('default')
+            .primaryPalette('customBlue', {
+                'default': '500',
+                'hue-1': '50'
+            })
+            .accentPalette('light-blue', {
+                'default': '500',
+                'hue-1': '50'
+            })
+    $mdThemingProvider.theme('input', 'default')
+            .primaryPalette('grey')
+});
+ 
